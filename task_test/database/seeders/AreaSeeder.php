538358428697fb7a258dcb8fb01a6c8bf76f7a35
Minjunghtj <105842738+Minjunghtj@users.noreply.github.com>
2022-06-21 14:37:04 +0900

    116.  リレーション（1対多）

---
+++ task_test/database/seeders/AreaSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AreaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('area')->insert([
+            [
+            'id' => 1,
+            'name' => '東京',
+            'sort_no' => 1   
+            ],
+            [
+            'id' => 2,
+            'name' => '大阪',
+            'sort_no' => 2 
+            ],
+            [
+            'id' => 3,
+            'name' => '福岡',
+            'sort_no' => 3
+
+            ]]
+            );
+
+    }
+}
